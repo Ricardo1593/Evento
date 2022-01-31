01c518047dfc873626b2ee6d13fa5950585c4060
Ricardo Sousa26 <Ricardo.Sousamoraes@hotmail.com>
2022-01-31 19:14:20 -0300

    ex finalizado

---
+++ evento.js
@@ -0,0 +1,37 @@
+let idade = 18
+
+if(idade >= 18) {
+    console.log("Premitir entrada apartir dos 18 anos")
+} else{ 
+  console.log("idade insuficiente")
+}
+//somente 100 pessoas são autorizadas
+
+  let NumeroDePessoas = 90
+
+  if("Quantidade maxima >= 100") {
+      console.log("Quantidade maxima de pessoas 100") 
+   } else  { 
+      console.log("quantidade máxima de pessoas atingida") 
+    } 
+     // data do inicio
+    
+     let  dados  =  20
+
+    consola . log ("A data para o evento é até o dia:", data.length )
+    
+    if ( data.length  >= 20 )  {
+        console.log ("continuar o cadastro")
+    }  else {
+        console.log ("Dados inválidos")
+    }
+     // lista de participantes
+    
+     if (lista)  = [ '' ]{
+ 
+    console.log (listarparticipantes.length)
+   }
+
+    else.listarpalestrantes  =  [ '' ]
+
+   console.log (listarpalestrantes.length)
